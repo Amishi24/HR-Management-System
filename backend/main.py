fa4eec7e3dca4f0d01b8f8dc76b3dc1000968562
Parth Sanghi <parthsanghi2001@gmail.com>
2026-06-10 22:18:45 +0530

    fix database connection

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from database import get_db_connection
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+from database import get_db
 
 app = FastAPI(title="ONGC API Server")
 
@@ -18,14 +20,19 @@ def read_root():
     return {"message": "ONGC FastAPI Backend Server is Running!"}
 
 @app.get("/test-db")
-def test_db():
+def test_db(db: Session = Depends(get_db)):
     try:
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute("SELECT NOW();")
-        db_time = cursor.fetchone()
-        cursor.close()
-        conn.close()
-        return {"message": "Supabase Database connected successfully!", "time": db_time[0]}
+        result = db.execute(text("SELECT NOW()")).fetchone()
+
+        return {
+            "message": "Database connected successfully!",
+            "time": str(result[0])
+        }
+
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Database connection failed: {str(e)}")
+        raise HTTPException(
+            status_code=500,
+            detail=f"Database connection failed: {str(e)}"
+        )
+    
+    
